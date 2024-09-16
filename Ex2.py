31909d58cd67565b64bb9bb87c53b2b5a80c9871
vlada890 <69687973+vlada890@users.noreply.github.com>
2024-09-16 10:41:04 +0300

    Mostenire

--- Ex2.py
+++ Ex2.py
@@ -1,21 +1,26 @@
-class Animal(object):
-    def __init__(self,age,name):
-        self.age=age
-        self.name=name
-    def __str__(self):
-        return str(self.name)+" are "+str(self.age)+" ani"
+#include <iostream>
+#include <string>
+using namespace std;
 
-class Student(Animal):
-    def __init__(self,age,name,licenta):
-        Animal.__init__(self,age,name)
-#instanta obiectului de clasa Student se initializeaza cu ajutorul atributelor clasei Animal prin mostenire
-        self.licenta=licenta
-    def speak(self):
-        print ("Salut!Invat la "+str(self.licenta))
-    def __del__(self):
-        print(str(self.name)+' a fost sters')
-#metoda str e mostenita de la superclasa Animal
-#exemplu:pers1=Student(21,'igor','programare') 
-#        print(persoana1) --> 'igor are 21 ani'
-#destructorul instantei clasei Student
-#exemplu: del pers1 --> 'igor a fost sters'
+// Base class
+class Persoana {
+  public: 
+    string name = "Ion";
+    void greet() {
+      cout << "Salut \n" ;
+    }
+};
+
+// Derived class
+class Student: public Persoana {
+  public: 
+    string facultate = "programare";
+};
+
+int main() {
+  Student obj1;
+  obj1.greet();
+  cout << obj1.name + " este de la " + obj1.facultate;
+  return 0;
+}
+
